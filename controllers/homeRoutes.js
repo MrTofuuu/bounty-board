6535412687af2292b18cfec30b52dd6ba5141e11
MrTofuuu <84428685+MrTofuuu@users.noreply.github.com>
2021-11-30 01:00:34 -0600

    reseeded data and posts are now showing correctly

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -16,10 +16,10 @@ router.get('/', async (req, res) => {
 
     // Serialize data so the template can read it
     const posts = postData.map((post) => post.get({ plain: true }));
-
+    console.log(posts)
     // Pass serialized data and session flag into template
     res.render('homepage', { 
-      ...posts, 
+      posts, 
       logged_in: req.session.logged_in 
     });
   } catch (err) {
