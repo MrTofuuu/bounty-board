fbdc41947a8ef77afb807f3fd62c74ac4794691e
MrTofuuu <84428685+MrTofuuu@users.noreply.github.com>
2021-11-30 00:42:07 -0600

    updated to spread data for rendering

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -19,7 +19,7 @@ router.get('/', async (req, res) => {
 
     // Pass serialized data and session flag into template
     res.render('homepage', { 
-      posts, 
+      ...posts, 
       logged_in: req.session.logged_in 
     });
   } catch (err) {
@@ -59,9 +59,9 @@ router.get('/profile', withAuth, async (req, res) => {
     });
 
     const user = userData.get({ plain: true });
-
+    console.log(user)
     res.render('profile', {
-      user,
+      ...user,
       logged_in: true
     });
   } catch (err) {
